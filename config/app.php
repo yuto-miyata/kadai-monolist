fdd90322795897b88a293b9e92eef3e475dec70a
yuto.miyata <yuto.miyata@forumeng.co.jp>
2018-04-27 01:12:48 +0000

    user login

--- config/app.php
+++ config/app.php
@@ -178,7 +178,8 @@ return [
         App\Providers\RouteServiceProvider::class,
         
         Collective\Html\HtmlServiceProvider::class,
-
+        
+        Thomaswelton\LaravelGravatar\LaravelGravatarServiceProvider::class,
     ],
 
     /*
@@ -228,7 +229,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Gravatar' => Thomaswelton\LaravelGravatar\Facades\Gravatar::class,
 
     ],
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,9 @@
         <dev class="cover-inner">
             <div class="cover-contents">
                 <hi>素敵なモノと出会う場所</hi>
-                <a href="" class="btn btn-success btn-lg">モノリストを始める</a>
+                @if (!Auth::check())
+                    <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg">モノリストを始める</a>
+                @endif
             </div>
         </dev>
     </div>
