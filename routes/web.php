7cc0a60b02e5693c0d81677445417a614661e8d7
yuto.miyata <yuto.miyata@forumeng.co.jp>
2018-05-01 02:31:06 +0000

    refresh

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,7 @@ Route::group(['middleware' => ['auth']], function() {
     
     Route::post('want', 'ItemUserController@want')->name('item_user.want');
     Route::delete('want', 'ItemUserController@dont_want')->name('item_user.dont_want');
-    Route::post('have', 'ItenUserController@have')->name('item_user.have');
+    Route::post('have', 'ItemUserController@have')->name('item_user.have');
     Route::delete('have', 'ItemUserController@dont_want')->name('item_user.dont_have');
     
     Route::resource('users', 'UsersController', ['only' => ['show']]);
