cfab417c27e2bcbf09197080f5b6a6a50ff46bd1
yuto.miyata <yuto.miyata@forumeng.co.jp>
2018-05-01 00:34:41 +0000

    ranking want

---
+++ app/Http/Controllers/RankingController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Item;
+
+class RankingController extends Controller
+{
+    public function want()
+    {
+        $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as count'))->whereRaw('type = "want"')->groupBy('items.id', 'items.code', 'items.name', 'items.url', 'items.image_url','items.created_at', 'items.updated_at')->orderBy('count', 'DESC')->take(10)->get();
+
+        return view('ranking.want', [
+            'items' => $items,
+        ]);
+    }
+}
