d96419b6400ecb9582ab0dff3f5faedb59bf26e5
yuto.miyata <yuto.miyata@forumeng.co.jp>
2018-05-08 07:33:52 +0000

    correction

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::group(['middleware' => ['auth']], function() {
     Route::post('want', 'ItemUserController@want')->name('item_user.want');
     Route::delete('want', 'ItemUserController@dont_want')->name('item_user.dont_want');
     Route::post('have', 'ItemUserController@have')->name('item_user.have');
-    Route::delete('have', 'ItemUserController@dont_want')->name('item_user.dont_have');
+    Route::delete('have', 'ItemUserController@dont_have')->name('item_user.dont_have');
     
     Route::resource('users', 'UsersController', ['only' => ['show']]);
 });
